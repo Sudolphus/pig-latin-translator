067adefa55ae0726555b2cf88f46ab8c07625dcd
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-10 11:22:33 -0700

    add js to handle words starting with vowels
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 //business logic
+const startTest = function(str) {
+  const vowels = 'AEIOUaeiou'.split('');
+  if (vowels.includes(str.charAt(0))) {
+    return vowelStart(str);
+  }
+}
 
+const vowelStart = function(str) {
+  return str + 'way';
+}
 
 //user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     const sentenceInput = $("input[name=sentence]").val();
+    const newSentence = startTest(sentenceInput);
+
+    $("#output").text(newSentence);
   });
 });
