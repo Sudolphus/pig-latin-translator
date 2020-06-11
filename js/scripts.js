c1fe1bfb25c2197029034a687396d4708a3b3f72
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-11 15:00:58 -0700

    add functionality to handle punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,32 @@ const pigLatinTranslate = function(sentence) {
 }
 
 const splitter = function(str) {
-  if (str.search(/[\W\d_]/) > -1) {
+  const stringArray = stripper(str)
+  if (stringArray[1].search(/[\W\d_]/) > -1) {
     return str;
   } else {
-    if (str.search(/[aeiou]/i) === 0) {
-      return str + 'way';
+    if (stringArray[1].search(/[aeiou]/i) === 0) {
+      return stringArray[0] + stringArray[1] + 'way' + stringArray[2];
     } else {
       const consonantBlock = /^[^aeiou]*((?<=q)u|(?!=u)[^aeiou])/i;
-      return str.split(consonantBlock)[str.split(consonantBlock).length-1] + str.match(consonantBlock)[0] + "ay";
+      return stringArray[0] + stringArray[1].split(consonantBlock)[stringArray[1].split(consonantBlock).length-1] + stringArray[1].match(consonantBlock)[0] + "ay" + stringArray[2];
     }
   }
+}
+
+const stripper = function(str) {
+  let newStr = str;
+  let begin = '';
+  let end = '';
+  while (newStr.search(/[\W\d_]/) === 0) {
+    begin += newStr.charAt(0);
+    newStr = newStr.slice(1);
+  }
+  while (newStr.search(/[\W\d_]$/) === newStr.length-1) {
+    end = newStr.charAt(newStr.length-1) + end;
+    newStr = newStr.slice(0, newStr.length-1);
+  }
+  return [begin, newStr, end];
 }
 
 //user interface logic
